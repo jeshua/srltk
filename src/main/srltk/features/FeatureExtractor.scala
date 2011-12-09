925ba877c6ee6bcadc3b8eaa37d03fd886470c54
Jeshua Bratman <jeshua@ishtar.(none)>
2011-12-08 22:41:13 -0500

    added invert function to feature extractor

--- src/main/srltk/features/FeatureExtractor.scala
+++ src/main/srltk/features/FeatureExtractor.scala
@@ -5,10 +5,11 @@ trait FeatureExtractor extends Serializable{
   def apply(feats: Feats): Feats 
   def length: Int
   def isSparse = false
+  def invert(feats : Feats) : Feats = null
 }
 
 class IdentityExtractor(outDim : Int) extends FeatureExtractor {
   def apply(input: Feats): Feats = input
-  def length: Int = outDim
+  def length: Int = outDim  
 }
 object IdentityExtractor { def apply(d: Int) = new IdentityExtractor(d) }
