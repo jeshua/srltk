0b3d914c823ffd162b353ab174646ee6810a78d9
Jeshua Bratman <jeshua@loki.(none)>
2012-08-18 22:13:39 -0400

    cartpole

--- src/main/scala/srltk/domains/rlstandard/CartPoleSwingup.scala
+++ src/main/scala/srltk/domains/rlstandard/CartPoleSwingup.scala
@@ -27,11 +27,11 @@ object CartPoleSwingup {
   val length = 2d;
   val mass_len_pole = mass_pole * length;
   val force_mag = 5.0d;//10
-  val tau = .04d; //0.02;  
-    
-  val start_x = 1
-  val start_deg = .2
+  val tau = .05d; //0.02;
   
+  val target_theta_thresh    = .1d;//.05 
+  val target_thetadot_thresh = .5d;//.5 
+      
   val max_x = 4d;
   val min_x = -4d;
   val max_v = 4d;
@@ -76,7 +76,9 @@ class CartPoleSwingupState(val x: Double, val xdot: Double, val theta: Double, v
   def getInitial = CartPoleSwingup.getInitial
   def isAbsorbing = succeed()
   
-  def succeed() = math.abs(math.abs(theta)-math.Pi) < .05 && math.abs(thetadot) < .5; 
+  def succeed() = {
+    math.abs(math.abs(theta)-math.Pi) < target_theta_thresh && math.abs(thetadot) < target_thetadot_thresh; 
+  }
 
   def copy = new CartPoleSwingupState(x, xdot, theta, thetadot);
 
